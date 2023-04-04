b9178a7d0a6fb1ee63224128da0679ec06c33e48
Ryan Lierman <rylierman@gmail.com>
2023-04-03 19:09:25 -0700

    fixed an issue

--- script.js
+++ script.js
@@ -58,15 +58,72 @@ function showWeatherData (data) {
   timezone.innerHTML = data.timezone;
   countryEl.innerHTML = data.lat + 'N ' + data.lon+'E';
 
+  let currentWeatherItems = `
+    <div class="weather-item">
+      <div>Humidity</div>
+      <div>${humidity}%</div>
+    </div>
+    <div class="weather-item">
+      <div>Pressure</div>
+      <div>${pressure}hPa</div>
+    </div>
+    <div class="weather-item">
+      <div>Wind Speed</div>
+      <div>${wind_speed}m/s</div>
+    </div>
+    <div class="weather-item">
+      <div>Sunrise</div>
+      <div>${moment(sunrise*1000).format('h:mm a')}</div>
+    </div>
+    <div class="weather-item">
+      <div>Sunset</div>
+      <div>${moment(sunset*1000).format('h:mm a')}</div>
+    </div>
+  `;
+
+  currentWeatherItemsEl.innerHTML = currentWeather
+}
+function showWeatherData (data) {
+  const humidity = data.current.humidity;
+  const pressure = data.current.pressure;
+  const sunrise = data.current.sunrise;
+  const sunset = data.current.sunset;
+  const wind_speed = data.current.wind_speed;
+
+  timezone.innerHTML = data.timezone;
+  countryEl.innerHTML = data.lat + 'N ' + data.lon+'E';
+
   currentWeatherItems
-  let otherDayForcast = '';
-  data.daily.forEach(function(day, idx) {
-      if(idx == 0) {
-          currentTempEl.innerHTML = '\n            <img src="http://openweathermap.org/img/wn//' + day.weather[0].icon + '@4x.png" alt="weather icon" class="w-icon">\n            <div class="other">\n                <div class="day">' + moment(day.dt*1000).format('dddd') + '</div>\n                <div class="temp">Night - ' + day.temp.night + '&#176;C</div>\n                <div class="temp">Day - ' + day.temp.day + '&#176;C</div>\n            </div>\n            ';
-      } else {
-          otherDayForcast += '\n            <div class="weather-forecast-item">\n                <div class="day">' + moment(day.dt*1000).format('ddd') + '</div>\n                <img src="http://openweathermap.org/img/wn/' + day.weather[0].icon + '@2x.png" alt="weather icon" class="w-icon">\n                <div class="temp">Night - ' + day.temp.night + '&#176;C</div>\n                <div class="temp">Day - ' + day.temp.day + '&#176;C</div>\n            </div>\n            ';
-      }
-  });
+// Initialize variable for storing other days' forecast HTML
+let otherDayForecastHTML = '';
+
+// Loop through daily weather data and generate HTML for current and other days
+data.daily.forEach(function(day, idx) {
+  if(idx == 0) {
+    // Generate HTML for current day
+    currentTempEl.innerHTML = `
+      <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png"
+        alt="weather icon" class="w-icon">
+      <div class="other">
+        <div class="day">${moment(day.dt*1000).format('dddd')}</div>
+        <div class="temp">Night - ${day.temp.night}&#176;C</div>
+        <div class="temp">Day - ${day.temp.day}&#176;C</div>
+      </div>
+    `;
+  } else {
+    // Generate HTML for other days and append to otherDayForecastHTML
+    otherDayForecastHTML += `
+      <div class="weather-forecast-item">
+        <div class="day">${moment(day.dt*1000).format('ddd')}</div>
+        <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"
+          alt="weather icon" class="w-icon">
+        <div class="temp">Night - ${day.temp.night}&#176;C</div>
+        <div class="temp">Day - ${day.temp.day}&#176;C</div>
+      </div>
+    `;
+  }
+});
+
 
   weatherForecastEl.innerHTML = otherDayForcast;
 }
